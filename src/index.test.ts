b28fafe34527a3badc9e95a696999eaf5efa5f36
dtinth on MBP M1 <dtinth@spacet.me>
2022-10-30 20:24:40 +0700

    Add tests and fix a bug

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,1 +1,43 @@
-it.todo('works')
+import { createServer } from 'http'
+import { isAxiosError } from './'
+
+const axios = require('axios/dist/node/axios.cjs')
+
+it('returns true for axios errors', async () => {
+  await withServer(async (url) => {
+    const error = await (async () => {
+      try {
+        await axios.get(url)
+      } catch (error) {
+        return error
+      }
+      throw new Error('Expected error')
+    })()
+    expect(isAxiosError(error)).toBe(true)
+    expect(isAxiosError(error, 404)).toBe(true)
+    expect(isAxiosError(error, 500)).toBe(false)
+  })
+})
+
+it('returns false for non-axios errors', async () => {
+  expect(isAxiosError(new Error('Whatever'))).toBe(false)
+  expect(isAxiosError(null)).toBe(false)
+  expect(isAxiosError(undefined)).toBe(false)
+  expect(isAxiosError(42)).toBe(false)
+})
+
+async function withServer(f: (url: string) => any) {
+  const server = createServer((req, res) => {
+    res.writeHead(404, { 'Content-Type': 'text/plain' })
+    res.end('Nope')
+  })
+  await new Promise<void>((resolve, reject) => {
+    server.listen(0, resolve).on('error', reject)
+  })
+  const url = `http://localhost:${(server.address() as any).port}`
+  try {
+    return await f(url)
+  } finally {
+    server.close()
+  }
+}
